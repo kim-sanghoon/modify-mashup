a8fbe56f6434370d2194be3529884bebb4a7b012
Sanghoon Kim <20144277+kim-sanghoon@users.noreply.github.com>
2020-11-17 17:45:11 +0900

    Update support for randomized instantiations

--- src/Action.py
+++ src/Action.py
@@ -1,3 +1,5 @@
+from random import randint
+
 action_dict = {}
 
 class NominalAction():
@@ -6,6 +8,7 @@ class NominalAction():
         self.inverseOf = json_info['inverseOf']
         self.type = json_info['type']
         self.values = json_info['values']
+        self.valuesRange = json_info['valuesRange'] if len(self.values) > 0 else {}
         self.language = json_info['language']
         self.mainEffect = []
         self.sideEffect = []
@@ -23,3 +26,15 @@ class InstantiatedAction():
         for k in _type.values:
             self.values[k] = _values[k]
     
+    def __init__(self, _type: NominalAction, _random: bool):
+        if not _random:
+            raise RuntimeError("You cannot instantiate actions without random flag!")
+
+        self.type = _type
+        self.values = {}
+        for k in _type.values:
+            minval, maxval = _type.valuesRange[k]
+            self.values[k] = randint(minval, maxval)
+    
+    def __repr__(self):
+        return '<{0} with {1}>'.format(self.type.name, self.values)
