93057bf50fe8769d56d50d3f34f68cb714356532
Sanghoon Kim <20144277+kim-sanghoon@users.noreply.github.com>
2021-03-23 14:27:38 +0900

    Add utils for parsing Dialogflow date-time.

---
+++ src/utils/datetimeParser.py
@@ -0,0 +1,20 @@
+from dateutil.parser import parse
+from ..Time import *
+
+def parseDate(isoString):
+    monthStr = [
+        'None', 'January', 'February', 'March', 'April',
+        'May', 'June', 'July', 'August', 'September', 
+        'October', 'November', 'December'
+    ]
+    date = parse(isoString)
+
+    return {
+        'month': monthStr[date.month],
+        'day': date.day
+    }
+
+def parseTime(isoString):
+    time = parse(isoString)
+
+    return Time(time.hour)
